03254479cc12903cfeb05c8a38b20f46c1492178
Thierry Michel <thmichel@gmail.com>
2018-08-11 22:07:22 +0200

    :tada: initial commit (snitchy != epic-snitchy)

---
+++ __tests__/index.test.js
@@ -0,0 +1,5 @@
+/* global it, expect */
+
+it('test Jest setup', () => {
+  expect(true).toBeTruthy();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
-/* eslint-disable global-require */
-/**
- * Webpack config builder
- *
- * @param {string} env - Environment, dev or prod
- * @returns {Object} - Environment specific configuration
- */
-function buildConfig(env) {
-  return require(`./config/${env}.js`)({
-    env,
-  })
+const WebpackStrip = require('webpack-strip');
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: isProd ? 'snitchy.min.js' : 'snitchy.js',
+    library: 'snitchy',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  mode: isProd ? 'production' : 'development',
+  optimization: {
+    minimize: isProd,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
+
+if (isProd) {
+  config.module.rules.push({
+    test: /\.js$/,
+    use: [
+      { loader: WebpackStrip.loader('debug', 'console.info') },
+    ],
+  });
 }
 
-module.exports = buildConfig
+module.exports = config;
